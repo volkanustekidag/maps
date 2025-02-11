5d332ae131b5a44272d7a6a1375466861c9c68f6
Volkan <56761000+volkanustekidag@users.noreply.github.com>
2025-02-11 21:21:53 +0300

    Initial Commit

---
+++ maps_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maps_app
+//
+//  Created by Volkan Ustekidag on 11.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ maps_appTests/maps_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  maps_appTests.swift
+//  maps_appTests
+//
+//  Created by Volkan Ustekidag on 11.02.2025.
+//
+
+import Testing
+@testable import maps_app
+
+struct maps_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
